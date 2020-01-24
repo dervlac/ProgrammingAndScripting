6664c44e3985821bae230a5ec58f071fe5998730
Dervla Candon <G00283361@gmit.ie>
2020-01-24 21:01:00 +0000

    added python version of BMI calculator file

---
+++ BMI_Calculator.py
@@ -0,0 +1,12 @@
+# This program calculates a person's BMI
+# Based on input height and weight
+# Height must be entered in cm.
+# Weight must be entered in kg.
+
+height = float(input("Please enter your height in cm: "))
+
+weight = float(input("Please enter your weight in kg: "))
+
+BMI = weight / ((height/100)**2)
+
+print("Your BMI is", BMI)
